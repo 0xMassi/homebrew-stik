b0080eba0df2e35ded01601e4803744150530d3f
Valerio <88933932+0xMassi@users.noreply.github.com>
2026-02-09 13:14:50 +0000

    Update Stik to v0.4.2

--- Casks/stik.rb
+++ Casks/stik.rb
@@ -1,9 +1,9 @@
 cask "stik" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.4.1"
-  sha256 arm:   "e9de518f71879a7ffcad49b86af2eb570a528b5a5001e3b74df1258c615b6fef",
-         intel: "6c1654ee60cf748c217b3cd21624b36e78a88842da98c4535d9c0044d6e48fc1"
+  version "0.4.2"
+  sha256 arm:   "e430a013b89a456d4fa0dad66a9523c71938dc4031ebf0a3531142e071273f88",
+         intel: "ec0558c8271472f069f9398008710565eef0461bfb987790ca8274ff7c3425ae"
 
   url "https://github.com/0xMassi/stik_app/releases/download/v#{version}/Stik_#{version}_#{arch}.dmg"
   name "Stik"
